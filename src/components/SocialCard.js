8e906d8d21418b1aeb945ffb149788e95bce7a5f
Critter4Dinner <woffordben1992@gmail.com>
2019-12-04 09:08:38 -0500

    added social card component and a html skeleton to begin styling

---
+++ src/components/SocialCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SocialCard extends React.Component{
+
+    render(){
+        
+        return (
+            <main className="the_card">
+                <blockquote>"When one teaches, two learn."</blockquote>
+                <cite>Robert Heilein</cite>
+            </main>
+        )
+    }
+}
+
+export default SocialCard;
