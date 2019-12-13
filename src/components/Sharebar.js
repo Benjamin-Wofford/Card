7f7b7d8fa90b2a289287cef722edc2c33f038423
Critter4Dinner <woffordben1992@gmail.com>
2019-12-13 13:34:23 -0500

    Moving on from this project to something more challenging

--- src/components/Sharebar.js
+++ src/components/Sharebar.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import './facebook.png';
+import fb from '../facebook.svg';
+import twitter from '../twitter.svg';
+import ig from '../instagram.svg';
 
 class Sharebar extends React.Component {
 
     render(){
         return (
         <div className='sharebar'>
-            <img className='fb' src='./facebook.png' alt='Facebook icon'></img>
-            <btn className='twitter'>Twitter</btn>
-            <btn className='ig'>Instagram</btn>
+            <img className='fb' src={fb} alt='Facebook icon'></img>
+            <img className='twitter' src={twitter} alt='Twitter icon'></img>
+            <img className='ig' src={ig} alt='Instagram icon'></img>
         </div>)
     }
 }
